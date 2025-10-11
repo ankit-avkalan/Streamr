78861155040cf48cc1190d36b96e2d3557949bf3
Godwin Orian <linusgodwin2@gmail.com>
2025-10-11 06:59:38 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -229,7 +229,7 @@ const App = () => {
 
               {/* all movies */}
               <section className="flex flex-col gap-4 sm:gap-6 px-4 sm:px-8 md:px-12">
-                <h3 className="text-xl sm:text-2xl md:text-3xl font-bold">All Movies</h3>
+                <h3 className="text-2xl sm:text-3xl md:text-4xl font-bold">All Movies</h3>
 
                 {isLoading ? (
                   <Spinner />
