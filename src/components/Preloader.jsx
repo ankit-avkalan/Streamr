ed261ca58541382878029c360c5f87d9f793578e
Godwin Orian <linusgodwin2@gmail.com>
2025-10-11 13:02:38 +0100

    Update Preloader.jsx

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -20,7 +20,7 @@ const Preloader = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{
               delay: index * 0.2,
-              duration: 0.2,
+              duration: 0.1,
               ease: "easeOut",
             }}
           >
